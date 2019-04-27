3a300e3a2659cfda1d54e6040ebed693228b7ae6
Akash <Akash@192.168.1.105>
2019-04-27 17:33:47 +0500

    Template Method Design Pattern With Diagram and Slides

---
+++ Template-Method-Design-Pattern/src/code/HousingClient.java
@@ -0,0 +1,17 @@
+package code;
+
+public class HousingClient {
+	public static void main(String[] args) {
+		System.out.println("Build a WoodenHouse\n");
+		HouseTemplate houseType = new WoodenHouse();
+	
+		// using template method
+		houseType.buildHouse();
+		System.out.println("************************************************************");
+	
+		System.out.println("Build a GlassHouse\n");
+		houseType = new GlassHouse();
+	
+		houseType.buildHouse();
+	}
+}
