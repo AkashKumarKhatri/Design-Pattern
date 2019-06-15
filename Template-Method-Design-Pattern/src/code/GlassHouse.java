a66c3468c0f774ae23dd70a7793a973732b1a602
Akash <kkatriakash@gmail.com>
2019-06-14 21:33:43 -0700

    Try

--- Template-Method-Design-Pattern/src/code/GlassHouse.java
+++ Template-Method-Design-Pattern/src/code/GlassHouse.java
@@ -6,7 +6,7 @@ public class GlassHouse extends HouseTemplate {
 	public void buildFoundation() {
 		System.out.println("Building foundation with cement,iron rods and sand");
 	}
-
+	// Khatri Test
 	@Override
 	public void buildPillars() {
 		System.out.println("Building Pillars with glass coating");
